ea82451086f453ead1be5eabd9d162dd3ddc709c
David Yell <dyell@ukwebmedia.com>
2013-04-18 10:48:19 +0100

    Took out the prompt as it was annoying on every page load

--- components/x-tag-helloworld/src/helloworld.js
+++ components/x-tag-helloworld/src/helloworld.js
@@ -5,7 +5,7 @@
 xtag.register('x-helloworld', {
     lifecycle: {
         created: function() {
-            var name = prompt("Your name is", "");
+            var name = 'Dave';
             this.innerHTML = 'Hello ' + name;
         }
     }
